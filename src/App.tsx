afcedd23e4f9956b9ce2ad6e75201e107c6b877f
HiJackCoke <dsa1520@naver.com>
2025-07-17 15:50:52 +0900

    Feat: Card component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
 import { Canvas } from "@react-three/fiber";
-import { Environment, Image, ScrollControls } from "@react-three/drei";
+import { Environment, ScrollControls } from "@react-three/drei";
 
 import Rig from "./components/Rig";
+import Card from "./components/Card";
 
 const App = () => {
   return (
     <Canvas camera={{ position: [0, 0, 100], fov: 15 }}>
       <ScrollControls pages={5} infinite>
         <Rig rotation={[0, 0, 0.15]}>
-          <Image url={`/img1_.jpg`} />
+          <Card
+            url={"/img1_.jpg"}
+            position={[0, 0, 1]}
+            rotation={[0, Math.PI, 0]}
+          />
         </Rig>
       </ScrollControls>
       <Environment preset="dawn" background blur={0.5} />
---
+++ src/components/Card.tsx
@@ -0,0 +1,59 @@
+import * as THREE from "three";
+import { useRef } from "react";
+import { Euler, ThreeEvent, useFrame, Vector3 } from "@react-three/fiber";
+import { Image } from "@react-three/drei";
+import { easing } from "maath";
+
+interface Props {
+  url: string;
+  position: Vector3;
+  rotation: Euler;
+}
+
+const Card = ({ url, position, rotation }: Props) => {
+  const ref = useRef<THREE.Mesh>(null);
+  const isHover = useRef(false);
+
+  const pointerOver = (e: ThreeEvent<PointerEvent>) => {
+    e.stopPropagation();
+    isHover.current = true;
+  };
+  const pointerOut = () => {
+    isHover.current = false;
+  };
+
+  useFrame((_, delta) => {
+    if (!ref.current) return;
+
+    easing.damp3(ref.current.scale, isHover.current ? 1.15 : 1, 0.1, delta);
+    easing.damp(
+      ref.current.material,
+      "radius",
+      isHover ? 0.25 : 0.1,
+      0.2,
+      delta
+    );
+    easing.damp(
+      ref.current.material,
+      "zoom",
+      isHover.current ? 1 : 1.5,
+      0.2,
+      delta
+    );
+  });
+
+  return (
+    <Image
+      ref={ref}
+      url={url}
+      transparent
+      side={THREE.DoubleSide}
+      onPointerOver={pointerOver}
+      onPointerOut={pointerOut}
+      position={position}
+      rotation={rotation}
+    />
+  );
+};
+
+export default Card;
