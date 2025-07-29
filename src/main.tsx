94622352e2ed950d0d16cb07f9a5298b79bf56c5
HiJackCoke <dsa1520@naver.com>
2025-07-29 14:05:33 +0900

    Feat: add router for page navigation

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+
 import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import routes from "vite-react-routes";
+
+// @ts-ignore
+const router = createBrowserRouter(routes());
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
